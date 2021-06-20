46a57946ea392dfb9f37d9cf02ca7b2aef384585
vitorwlima <vitorwlima13@gmail.com>
2021-06-20 16:24:37 -0300

    first commit with initial config and routing system

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+
+  button {
+    font-size: inherit;
+  }
+`
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Wrapper } from './styles'
+
+const index = () => {
+  return <Wrapper>página home</Wrapper>
+}
+
+export default index
